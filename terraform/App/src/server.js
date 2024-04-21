52679001a93ee81bbe7c684897c0d231e2413cfc
jenkins <nthoangthcs@gmail.com>
2024-04-22 02:09:45 +0700

    chore: update terraform

--- terraform/App/src/server.js
+++ terraform/App/src/server.js
@@ -7,29 +7,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-// Hard-Coded for demo
-const CONTACTS = [
-                    {
-                      "name": "Foo Bar",
-                      "email": "foobar@test.com",
-                      "cell": "555-123-4567"
-                    },
-                    {
-                      "name": "Biz Baz",
-                      "email": "bizbaz@test.com",
-                      "cell": "555-123-5678"
-                    },
-                    {
-                      "name": "Bing Bang",
-                      "email": "bingbang@test.com",
-                      "cell": "555-123-6789"
-                    }
-                ];
-
-app.get('/contacts', (req, res) => {
-  res.json({contacts: CONTACTS});
-});
 
 app.listen(3000, () =>{
-   console.log('Server running on port 3000.'); 
+   console.log('Welcome warriors to Golden Owl!'); 
 });
